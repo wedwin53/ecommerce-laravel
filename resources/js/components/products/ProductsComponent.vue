2655c1f18682446ea35d83df237d8a6944348b9e
wedwin53 <wedwin53@gmail.com>
2018-11-12 19:09:16 -0600

    Iniciando con Vue.js

---
+++ resources/js/components/products/ProductsComponent.vue
@@ -0,0 +1,14 @@
+<template lang="html">
+    <p>Mi Primer componente Vue</p>
+</template>
+<script>
+
+export default {
+    data(){
+        return{
+            name: 'products Component'
+        }
+    }
+}
+</script>
+
